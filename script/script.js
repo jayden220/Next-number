3ba96edb3c6ad1271d9b94f393bade1b7d6810b4
jayden220 <jaydenvanrooyen87@gmail.com>
2024-06-05 10:07:41 +0200

    final changes

--- script/script.js
+++ script/script.js
@@ -1,25 +1,26 @@
-
+let nextNum = document.querySelector('button')
+let numberDisplay = document.getElementById('numberDisplay')
 let arr = [1, 12, 26, 48, 85];
+let iteration = 0;
+let maxValue = arr.length;
 
 function getNext() {
-    let iteration = 0;
-    let maxValue = arr.length;
 
-    return () => {
+    
         let number = arr[iteration];
         iteration++;
         if (iteration === maxValue) iteration = 0;
         return number;
     };
-}
-
-let getNextNumber = getNext();
-
-console.log(getNextNumber()); 
-console.log(getNextNumber()); 
-console.log(getNextNumber()); 
-console.log(getNextNumber()); 
-console.log(getNextNumber()); 
 
 
- 
+
+    nextNum.addEventListener('click',function () {
+        
+        let getNextNumber = getNext();
+        numberDisplay.innerHTML = getNextNumber;
+    })
+
+
+
+
